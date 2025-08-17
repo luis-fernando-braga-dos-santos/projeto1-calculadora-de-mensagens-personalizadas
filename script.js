a6712460c10f8c006f150a668657f7b71472cb22
luis-fernando-braga-dos-santos <luis.santos295@aluno.ce.gov.br>
2025-08-17 09:04:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const nome = "luis"
+let idade = 16
+let cidade = "apuiares"
+let profissao = "desenvolvedor"
+let anoatual = 2025
+let anonascimento = anoatual-idade
+let salario = 4000
+let bonus = 500
+let totalMensal = 4500
+let totalAno = 48000
+let totalAnualdobrado = 48000 * 2
+alert(`
+========== RELATORIO DO USUARIO ==========
+nome: ${nome}
+idade: ${idade}
+Ano de Nascimento: ${anonascimento}
+cidade: ${cidade}
+profissao: ${profissao}
+
+salario base: ${salario}
+bonus: ${bonus}
+total mensal: ${totalMensal}
+total ano: ${totalAno}
+total anual (salario dobrado): ${totalAnualdobrado}
+===========================================`)
